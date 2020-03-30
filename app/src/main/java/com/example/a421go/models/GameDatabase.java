44e80ffdcc17bb99690ad436fae3295c69d757f1
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2020-03-30 14:10:57 +0200

    Transformation de GameDatabase en singleton

--- app/src/main/java/com/example/a421go/models/GameDatabase.java
+++ app/src/main/java/com/example/a421go/models/GameDatabase.java
@@ -5,17 +5,39 @@ import android.database.sqlite.SQLiteDatabase;
 
 import com.example.a421go.metier.SQLiteManager;
 
+/**
+ * Représente la base de données du jeu.
+ */
 public class GameDatabase {
     private String name = "gameDB.sqlite";
     private int version = 1;
     private SQLiteManager manager;
     private SQLiteDatabase content;
+    private static GameDatabase instance = null;
 
     /**
      * Le constructeur de la classe.
      * @param context le contexte.
      */
-    public GameDatabase(Context context) {
+    protected GameDatabase(Context context) {
         manager = new SQLiteManager(context, name, null, version);
     }
+
+    /**
+     * Si aucune instance n'existe, crée une instance de la classe. Sinon,
+     * retourne l'instance existante.
+     * @return L'unique instance de la classe.
+     */
+    public static GameDatabase getInstance(Context context) {
+        if (GameDatabase.instance == null) {
+            GameDatabase.instance = new GameDatabase(context);
+        }
+        GameDatabase db = new GameDatabase(context);
+        return instance;
+    }
+
+//    public void addPlayer(Player player) {
+//        content = manager.getWritableDatabase();
+//        String req = "INSERT INTO "
+//    }
 }
