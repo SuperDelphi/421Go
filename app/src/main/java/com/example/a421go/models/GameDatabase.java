a6be1210b18724476ff42f1939b0fbebe1112aac
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2020-03-30 14:38:02 +0200

    Suppression du singleton de GameDatabase

--- app/src/main/java/com/example/a421go/models/GameDatabase.java
+++ app/src/main/java/com/example/a421go/models/GameDatabase.java
@@ -13,27 +13,13 @@ public class GameDatabase {
     private int version = 1;
     private SQLiteManager manager;
     private SQLiteDatabase content;
-    private static GameDatabase instance = null;
 
     /**
      * Le constructeur de la classe.
      * @param context le contexte.
      */
-    protected GameDatabase(Context context) {
+    public GameDatabase(Context context) {
         manager = new SQLiteManager(context, name, null, version);
-    }
-
-    /**
-     * Si aucune instance n'existe, crée une instance de la classe. Sinon,
-     * retourne l'instance existante.
-     * @return L'unique instance de la classe.
-     */
-    public static GameDatabase getInstance(Context context) {
-        if (GameDatabase.instance == null) {
-            GameDatabase.instance = new GameDatabase(context);
-        }
-        GameDatabase db = new GameDatabase(context);
-        return instance;
     }
 
 //    public void addPlayer(Player player) {
