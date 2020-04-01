58c445cb977e242c0886fd3b37597661a4a6d2d3
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-04-01 16:20:50 +0200

    Ajout des fonction de contrôles des combinaisons possible obtenu

--- app/src/main/java/com/example/a421go/controllers/BoardController.java
+++ app/src/main/java/com/example/a421go/controllers/BoardController.java
@@ -36,22 +36,40 @@ public class BoardController extends Controller {
         return instance;
     }
 
-
+    /**
+     * Fonction qui récupère les trois dés du jeu, les trie dans l'ordre croissant et cherche la combinaison obtenu (s'il y en a une)
+     * @return la liste des dés dans une arraylist de dice
+     */
     public ArrayList<Dice> searchDices(){//Dice dice1, Dice dice2, Dice dice3){
         ArrayList<Dice> dicesList = new ArrayList<Dice>();
-        Dice dice1 = new Dice(6);Dice dice2 = new Dice(2);Dice dice3 = new Dice(4);
+        // Dés de test (retirer asap)
+        Dice dice1 = new Dice(1);Dice dice2 = new Dice(2);Dice dice3 = new Dice(4);
         dicesList.add(dice1);dicesList.add(dice2);dicesList.add(dice3);
         Collections.sort(dicesList, new DiceComparator());
+        if (search421(dicesList)){
+            //421
+        } else if (searchFiche(dicesList)){
+            //fiche
+        } else if (searchBaraque(dicesList)){
+            //baraque
+        } else if (searchSuite(dicesList)) {
+            //suite
+        } else {
+            //autre
+        }
         return dicesList;
     }
 
 
+    /**
+     * Cherche s'il y a la combianison 421
+     * @param dicesList liste des dés trié dans l'ordre croissant
+     * @return vrai s'il y a la combinaison, sinon faux
+     */
     public boolean search421(ArrayList<Dice> dicesList){
-        if (Collections.min(dicesList).getFace() == 1){
-            dicesList.remove(0);
-            if (Collections.min(dicesList).getFace() == 2){
-                dicesList.remove(0);
-                if (Collections.min(dicesList).getFace() == 4){
+        if (dicesList.get(0).getFace() == 1){
+            if (dicesList.get(1).getFace() == 2){
+                if (dicesList.get(2).getFace() == 4){
                     return true;
                 } else {
                     return false;
@@ -62,9 +80,55 @@ public class BoardController extends Controller {
         } else {
             return false;
         }
+
+
+    }
+
+    /**
+     * Recherche s'il y a la combinaison "Fiche" soit 2 dés 1 et un autre dés quelconque
+     * @param dicesList liste des dés trié dans l'ordre croissant
+     * @return vrai s'il y a la combianison sinon faux
+     */
+    public boolean searchFiche(ArrayList<Dice> dicesList){
+        if (dicesList.get(0).getFace() == 1 && dicesList.get(1).getFace() == 1){
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
+    /**
+     * Recherche la combinaison "Baraque" soit 3 dés de même face
+     * @param dicesList liste des dés trié dans l'ordre croissant
+     * @return vrai s'il y a la combianison sinon faux
+     */
+    public boolean searchBaraque(ArrayList<Dice> dicesList){
+        if (dicesList.get(0).getFace() == dicesList.get(1).getFace()){
+            if (dicesList.get(1).getFace() == dicesList.get(2).getFace()){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
-
+    /**
+     * Recherche la combinaison "Suite" soit 3 dés dont les faces se suivent
+     * @param dicesList liste des dés trié dans l'ordre croissant
+     * @return vrai s'il y a la combianison sinon faux
+     */
+    public boolean searchSuite(ArrayList<Dice> dicesList){
+        if (dicesList.get(0).getFace() == dicesList.get(1).getFace()-1){
+            if (dicesList.get(1).getFace() == dicesList.get(2).getFace()-1){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
