e515361583a0d40de9c6ab6866cf6fdce4e84dfe
StarMaster007 <55792130+StarMaster007@users.noreply.github.com>
2020-04-03 15:12:08 +0200

    MAJ GameActivity
    - Ajout des objets de la vue activity_game

--- app/src/main/java/com/example/a421go/views/GameActivity.java
+++ app/src/main/java/com/example/a421go/views/GameActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.a421go.R;
 import com.example.a421go.controllers.BoardController;
@@ -21,6 +22,11 @@ public class GameActivity extends AppCompatActivity {
     private BoardController boardController;
     private GameController gameController;
     private SimpleBoard board;
+    private TextView playergameTV;
+    private TextView remainingThrowsTV;
+    private Button gameInfoBTN;
+    private Button rollBTN;
+    private LinearLayout boardLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +36,16 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void init() {
-        this.boardController = BoardController.getInstance(this);
-        this.board.addDice(new Dice()).addDice(new Dice()).addDice(new Dice());
+        boardController = BoardController.getInstance(this);
+        board.addDice(new Dice()).addDice(new Dice()).addDice(new Dice());
+        playergameTV = (TextView) findViewById(R.id.playergameTV);
+        remainingThrowsTV = (TextView) findViewById(R.id.remainingThrowsTV);
+        gameInfoBTN = (Button) findViewById(R.id.gameInfoBTN);
+        rollBTN = (Button) findViewById(R.id.rollBTN);
+        boardLayout = (LinearLayout) findViewById(R.id.boardLayout);
     }
 
     private void listenRollDices() {
-        Button rollBTN = (Button) findViewById(R.id.rollBTN);
         rollBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
