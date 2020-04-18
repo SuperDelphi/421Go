ce69e84dcae6923554bc0d6e886ae7cd8f904f83
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2020-04-18 17:03:47 +0200

    Documentation du dossier lib/

--- app/src/main/java/com/example/a421go/lib/DimensionConverter.java
+++ app/src/main/java/com/example/a421go/lib/DimensionConverter.java
@@ -3,7 +3,16 @@ package com.example.a421go.lib;
 import android.content.Context;
 import android.util.TypedValue;
 
+/**
+ * Représente un convertisseur de dimensions.
+ */
 public class DimensionConverter {
+    /**
+     * Convertit une dimension en pixels en DPI.
+     * @param context le contexte
+     * @param value la valeur en pixels à convertir
+     * @return la valeur convertie en DPI
+     */
     public static int convertPixelToDP(Context context, int value) {
         int dimensionInDP = (int) TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
--- app/src/main/java/com/example/a421go/lib/VibratorHelper.java
+++ app/src/main/java/com/example/a421go/lib/VibratorHelper.java
@@ -7,7 +7,14 @@ import android.os.Vibrator;
 
 import com.example.a421go.metier.GlobalApplication;
 
+/**
+ * Classe permettant d'accéder plus simplement à la fonction de vibration du terminal de l'utilisateur.
+ */
 public class VibratorHelper {
+    /**
+     * Fait vibrer le terminal de l'utilisateur pendant une durée définie.
+     * @param duration la durée (en millisecondes)
+     */
     public static void vibrate(int duration) {
         Vibrator v = (Vibrator) GlobalApplication.getAppContext().getSystemService(Context.VIBRATOR_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
